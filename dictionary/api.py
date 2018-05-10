788f3c3af40f38adcc452e05be90e4dee81c3391
kmhoran <kevin.michael.horan@gmail.com>
2018-05-09 20:52:48 -0700

    begin building custom posting methods

--- dictionary/api.py
+++ dictionary/api.py
@@ -1,3 +1,4 @@
+from django.db.utils import Error
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 
@@ -8,6 +9,11 @@ class WordViewSet(ModelViewSet):
     # Get all words from DB
     queryset = Word.objects.all()
     serializer_class = WordSerializer
+
+    def create(self, request):
+        serializer = WordSerializer(data=request.data)
+        if serializer.is_valid():
+            raise Error()
 
 
 class DefinitionViewSet(ModelViewSet):
